e33181f15d13e77415189b201c699f5d49692121
Oscar <o@ovl.design>
2025-03-13 20:50:12 +0100

    feat(index): add content

---
+++ _src/pages/index.11ty.js
@@ -0,0 +1,43 @@
+class Home {
+  data() {
+    return {
+      layout: 'home',
+      permalink: '/',
+      templateClass: 'page-home',
+      pageCSS: 'home.css',
+    }
+  }
+
+  renderGroups(allGroups, groupsWithContent) {
+    return `<ol class="group-list">
+      ${allGroups
+        .map((g) => {
+          if (groupsWithContent.includes(g.id)) {
+            return `<li><a href="/${this.slugify(g.name)}/">${g.name}</a></li>`
+          }
+
+          return `<li>${g.name}</li>`
+        })
+        .join('')}
+    </ol>`
+  }
+
+  render({ series, collections }) {
+    return `<section class="home-intro">
+      <p class="text-centered">
+        Hunderte guter und erprobter Ratschläge – vom Einkaufen bis zur vollendeten
+        Speisezubereitung – finden Sie in 256 Karten übersichtlich in 25 Gruppen
+        gegliedert.
+      </p>
+      ${this.renderGroups(series, collections.seriesWithEntries)}
+      <p class="text-centered">
+        <small>
+          Bitte beachten Sie auch die auf der Rückseite gegebenen Hinweise!
+        </small>
+      </p>
+    </section>
+  `
+  }
+}
+
+module.exports = Home
