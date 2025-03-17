166654a60857688e0dd0a0c3dd22226fa187ce7e
Oscar <o@ovl.design>
2025-03-17 09:12:56 +0100

    feat(podcast): add platform data

---
+++ _src/_data/podcast.mjs
@@ -0,0 +1,14 @@
+export default {
+  platforms: [
+    {
+      id: 1,
+      name: 'Apple Podcasts',
+      url: 'https://podcasts.apple.com/de/podcast/zeichen-guter-gastlichkeit/id1802482567',
+    },
+    {
+      id: 50,
+      name: 'Spotify',
+      link: 'https://open.spotify.com/show/3eV43iLgMkbYNbgXFPA8x9',
+    },
+  ],
+}
