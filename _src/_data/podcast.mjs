1e39fc0c2c760511594a9457cf423665bc48e23c
Oscar <o@ovl.design>
2025-03-18 10:29:48 +0100

    feat(podcast): add additional hosts

--- _src/_data/podcast.mjs
+++ _src/_data/podcast.mjs
@@ -6,6 +6,21 @@ export default {
       url: 'https://podcasts.apple.com/de/podcast/zeichen-guter-gastlichkeit/id1802482567',
     },
     {
+      id: 20,
+      name: 'Pocket Casts',
+      link: 'https://play.pocketcasts.com/podcasts/76c0a680-e2fa-013d-3be8-0affe7723725',
+    },
+    {
+      id: 35,
+      name: 'Deezer',
+      link: 'https://www.deezer.com/de/show/1001723921',
+    },
+    {
+      id: 40,
+      name: 'RSS',
+      link: 'https://gastlichkeit.ovl.design/feeds/podcast.rss',
+    },
+    {
       id: 50,
       name: 'Spotify',
       link: 'https://open.spotify.com/show/3eV43iLgMkbYNbgXFPA8x9',
