8fbc4ad85bc348b0379d6a042f435ad2665779c1
Oscar <o@ovl.design>
2025-05-26 23:31:54 +0200

    feat(podcast): add script to create podcast image

---
+++ _helper/create-podcast-episode-image.js
@@ -0,0 +1,24 @@
+import sharp from 'sharp'
+import path from 'path'
+import { getAllCardImages, srcImagesDir } from './get-all-card-images.js'
+
+const cwd = process.cwd()
+
+export async function createPodcastImage(image) {
+  const fileName = image.replace('.jpg', '-podcast.jpg')
+  const outputPath = path.resolve(cwd, '_src/assets/img/podcast', fileName)
+
+  await sharp(path.resolve(srcImagesDir, image))
+    .resize(2000, 2000)
+    .toFile(outputPath)
+
+  console.log(`🖼️ Created podcast episode image for ${image}`)
+}
+
+export async function createPodcastEpisodeImages() {
+  const images = await getAllCardImages()
+
+  const promises = images.map((image) => createPodcastImage(image))
+
+  await Promise.all(promises)
+}
