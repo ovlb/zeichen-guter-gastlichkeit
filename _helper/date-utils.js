96724ad8e1ecab972617413996a522abe9ed1a6d
Oscar <o@ovl.design>
2025-06-01 15:52:06 +0200

    feat(dates): simplify date utils

--- _helper/date-utils.js
+++ _helper/date-utils.js
@@ -1,21 +1,24 @@
 export const dateUtils = {
+  formatter: new Intl.DateTimeFormat('de-DE', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }),
+
   isWeekend(date) {
     const day = date.getDay()
     return day === 0 || day === 6 // 0 is Sunday, 6 is Saturday
   },
 
   format(date) {
-    return new Intl.DateTimeFormat('de-DE', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    }).format(date)
+    return this.formatter.format(date)
   },
 
   formatYYYYMMDD(date) {
     const year = date.getFullYear()
     const month = String(date.getMonth() + 1).padStart(2, '0')
     const day = String(date.getDate()).padStart(2, '0')
+
     return `${year}-${month}-${day}`
   },
 
@@ -23,19 +26,14 @@ export const dateUtils = {
     const nextDate = new Date(date)
     const day = date.getDay()
 
-    // Add one day by default
-    let daysToAdd = 1
-
-    if (day === 5) {
-      // Friday -> skip to Monday
-      daysToAdd = 3
-    } else if (day === 6) {
-      // Saturday -> skip to Monday (+2 days)
-      daysToAdd = 2
-    }
-    // For Sunday (day === 0) and weekdays, we already have daysToAdd = 1
+    const daysToAdd =
+      new Map([
+        [5, 3], // Friday -> skip to Monday (+3 days)
+        [6, 2], // Saturday -> skip to Monday (+2 days)
+      ]).get(day) || 1 // Default to 1 day for Sunday and weekdays
 
     nextDate.setDate(date.getDate() + daysToAdd)
+
     return nextDate
   },
 }
