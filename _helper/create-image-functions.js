4779c75e6e93cd488978e97a06a3091dea860a89
Oscar <o@ovl.design>
2025-06-10 21:51:55 +0200

    fix(cards): wrongly generating tiff images for content

--- _helper/create-image-functions.js
+++ _helper/create-image-functions.js
@@ -4,28 +4,32 @@ import { getAllCardImages, srcImagesDir } from './get-all-card-images.js'
 
 const cwd = process.cwd()
 
-export async function createPodcastImage(image) {
-  const fileName = image.replace('.jpg', '-podcast.jpg')
+export async function createPodcastImage(sourceFileName) {
+  const fileName = sourceFileName.replace('.tiff', '-podcast.jpg')
   const outputPath = path.resolve(cwd, '_src/assets/img/podcast', fileName)
 
-  await sharp(path.resolve(srcImagesDir, image))
+  await sharp(path.resolve(srcImagesDir, sourceFileName))
+    .jpeg({
+      quality: 100,
+    })
     .resize(2000, 2000)
     .toFile(outputPath)
 
-  console.log(`🖼️ 🎤 Created podcast episode image for ${image}`)
+  console.log(`🖼️ 🎤 Created podcast episode image for ${sourceFileName}`)
 }
 
-export async function createContentImage(imageFileName) {
-  const outputPath = path.resolve(cwd, '_src/assets/img/cards', imageFileName)
+export async function createContentImage(sourceFileName) {
+  const fileName = sourceFileName.replace('.tiff', '.jpg')
+  const outputPath = path.resolve(cwd, '_src/assets/img/cards', fileName)
 
-  await sharp(path.resolve(srcImagesDir, imageFileName))
+  await sharp(path.resolve(srcImagesDir, sourceFileName))
     .jpeg({
       quality: 100,
     })
     .resize(2000)
     .toFile(outputPath)
 
-  console.log(`🖼️ 📔 Created content image for ${imageFileName}`)
+  console.log(`🖼️ 📔 Created content image for ${sourceFileName}`)
 }
 
 export async function createPodcastEpisodeImages() {
